05e99f30b33a97e1f369290d60f61d1cdc56cfab
Ricardo Alvarez <mtzricardo765@gmail.com>
2025-09-01 12:27:04 -0600

    facturaRepository: se agrego consulta personalizada

--- src/main/java/org/curso/data/jpa/ejemplo/springjpa/repositories/FacturaReporsitory.java
+++ src/main/java/org/curso/data/jpa/ejemplo/springjpa/repositories/FacturaReporsitory.java
@@ -2,6 +2,11 @@ package org.curso.data.jpa.ejemplo.springjpa.repositories;
 
 import org.curso.data.jpa.ejemplo.springjpa.entities.Factura;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FacturaReporsitory extends JpaRepository<Factura, Long> {
+
+    //haciendo un join fetch para mejorar las consultas y no hacerlas con lazy
+    @Query("SELECT f FROM Factura f join fetch f.cliente c join fetch f.items i join fetch i.producto WHERE f.id =?1")
+     public Factura fetchByIdWithClienteWithItemFacturaWithProducto(Long id);
 }
