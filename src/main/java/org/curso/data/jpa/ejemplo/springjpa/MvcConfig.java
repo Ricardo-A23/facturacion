c2f25478a292e4047908e1d0730f7f7e87ff1124
Ricardo Alvarez <mtzricardo765@gmail.com>
2025-09-02 12:32:35 -0600

    se agrego un viewController que se encarga de mostrar vistas estaticas

--- src/main/java/org/curso/data/jpa/ejemplo/springjpa/MvcConfig.java
+++ src/main/java/org/curso/data/jpa/ejemplo/springjpa/MvcConfig.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,9 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
        registry.addResourceHandler("/uploads/**").addResourceLocations("file:uploads");
+    }
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/error_403").setViewName("error_403");
     }
 }
